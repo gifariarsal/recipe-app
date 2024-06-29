8036e810a1c078a4d057080fa3350b33ba389e98
gifariarsal <117580307+gifariarsal@users.noreply.github.com>
2024-06-29 22:50:30 +0800

    feat: add background image to index page, modify styles, update metadata, and change application name

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,33 @@
-import Image from "next/image";
-import Link from "next/link";
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Recipe App</h1>
-      <Link href="/recipes"></Link>
+    <div className="relative min-h-screen w-full mx-auto flex flex-col justify-center items-center">
+      <div
+        className="absolute inset-0 opacity-25"
+        style={{
+          backgroundImage: 'url(/bg.jpg)',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          zIndex: -1,
+        }}
+      />
+      <h1 className="text-2xl lg:text-4xl font-bold text-gray-800 mb-2">
+        Recipe App
+      </h1>
+      <p className="w-2/3 text-center text-gray-700 font-medium mb-10">
+        Discover Recipe Realm, your gateway to culinary exploration! Unleash
+        your inner chef with easy-to-follow recipes and diverse dishes that will
+        inspire your kitchen creativity.
+      </p>
+      <Link href="/recipes">
+        <Button size="lg">
+          <label className="md:text-lg cursor-pointer">Explore Recipes</label>
+        </Button>
+      </Link>
     </div>
   );
 }
--- src/components/recipe-details/index.js
+++ src/components/recipe-details/index.js
@@ -1,10 +1,13 @@
-import Link from "next/link";
+import Link from 'next/link';
+import { Button } from '../ui/button';
 
 const RecipeDetails = ({ image, name, ingredients, mealType, cuisine }) => {
   return (
-    <div>
-      <Link href={'/recipes'}>Go to recipe list</Link>
-      <div className="p-6 lg:max-w-6xl max-w-2xl mx-auto">
+    <div className="p-8 mx-auto lg:max-w-6xl md:max-w-4xl sm:max-w-full">
+      <Link href={'/recipes'}>
+        <Button>Back</Button>
+      </Link>
+      <div className='mt-6'>
         <div className="grid items-start grid-cols-1 lg:grid-cols-2 gap-10">
           <div className="w-full lg:sticky top-0 sm:flex gap-2">
             <img
@@ -23,11 +26,11 @@ const RecipeDetails = ({ image, name, ingredients, mealType, cuisine }) => {
             </div>
             <div className="mt-5">
               <h3 className="text-lg font-bold text-gray-700">Ingredients</h3>
-              <ul className="space-y-3 list-disc mt-4 pl-4 text-sm text-gray-700">
+              <ol className="space-y-3 list-decimal mt-4 pl-4 text-sm text-gray-700">
                 {ingredients.map((item) => (
                   <li key={item}>{item}</li>
                 ))}
-              </ul>
+              </ol>
             </div>
           </div>
         </div>
